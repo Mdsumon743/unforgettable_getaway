509dfc961380b4ac8d811ff70275ddf95e560ea7
mdsumon743 <mdsumonhossan743@gmail.com>
2024-12-04 09:44:50 +0600

    ............

--- lib/app.dart
+++ lib/app.dart
@@ -6,7 +6,7 @@ import 'package:unforgettable_getaway/core/test.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
