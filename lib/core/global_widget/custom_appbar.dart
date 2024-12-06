8985c4bbfbc54db346abc9602104d4f100ee4e04
mdsumon743 <mdsumonhossan743@gmail.com>
2024-12-06 14:36:50 +0600

    ...

--- lib/core/global_widget/custom_appbar.dart
+++ lib/core/global_widget/custom_appbar.dart
@@ -13,6 +13,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      toolbarHeight: 165.h,
       backgroundColor: AppColors.darkBrown,
       leading: const SizedBox(),
       shape: RoundedRectangleBorder(
@@ -92,5 +93,5 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(160.h);
+  Size get preferredSize => Size.fromHeight(165.h);
 }
