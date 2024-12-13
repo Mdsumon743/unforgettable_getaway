a9854ad5eeb7a9eac8b50a1da00c5a9eb9260b29
mdsumon743 <mdsumonhossan743@gmail.com>
2024-12-13 18:08:54 +0600

    class

--- lib/feature/profile/presentation/widget/favorited.dart
+++ lib/feature/profile/presentation/widget/favorited.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:unforgettable_getaway/core/global_widget/custom_text_popins.dart';
+import 'package:unforgettable_getaway/feature/profile/presentation/screen/favorites_list.dart';
 
 Widget fovoritedMe(
   String count,
   String text,
 ) {
-  return Column(
-    children: [
-      CustomTextPopins(
-        text: count,
-        fontWeight: FontWeight.w600,
-        size: 24.sp,
-        color: Colors.white,
-      ),
-      SizedBox(
-        height: 5.h,
-      ),
-      CustomTextPopins(
-        text: text,
-        fontWeight: FontWeight.w400,
-        size: 16.sp,
-        color: Colors.white.withOpacity(0.85),
-      ),
-    ],
+  return GestureDetector(
+    onTap: () {
+      Get.to(() => const FavoritesList());
+    },
+    child: Column(
+      children: [
+        CustomTextPopins(
+          text: count,
+          fontWeight: FontWeight.w600,
+          size: 24.sp,
+          color: Colors.white,
+        ),
+        SizedBox(
+          height: 5.h,
+        ),
+        CustomTextPopins(
+          text: text,
+          fontWeight: FontWeight.w400,
+          size: 16.sp,
+          color: Colors.white.withOpacity(0.85),
+        ),
+      ],
+    ),
   );
 }
