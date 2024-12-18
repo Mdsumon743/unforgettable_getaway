3026ffe4df9799f6b6d6d182b24043b92f60904b
mdsumon743 <mdsumonhossan743@gmail.com>
2024-12-18 17:38:07 +0600

    firebase for notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -5,12 +6,16 @@ import 'package:unforgettable_getaway/app.dart';
 import 'package:unforgettable_getaway/core/binding/binding.dart';
 import 'package:unforgettable_getaway/core/helper/shared_prefarences_helper.dart';
 import 'package:unforgettable_getaway/feature/onboarding/controller/onboarding_controller.dart';
+import 'package:unforgettable_getaway/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   AppBingding().dependencies();
   OnboardingController().onInit();
   await SharedPreferencesHelper().init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
