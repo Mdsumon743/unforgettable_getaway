24e80663872538bbd25983ad040f3fd3ed3ee256
mdsumon743 <mdsumonhossan743@gmail.com>
2024-12-18 10:00:45 +0600

    push

--- lib/feature/auth/controller/social_login.dart
+++ lib/feature/auth/controller/social_login.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:unforgettable_getaway/core/network_caller/service/service.dart';
+import 'package:unforgettable_getaway/core/network_caller/utils/utils.dart';
+
+import '../../../core/helper/shared_prefarences_helper.dart';
 
 class SocialLogin extends GetxController {
+  SharedPreferencesHelper preferencesHelper = SharedPreferencesHelper();
   Future<void> googleSignIn() async {
     GoogleSignIn googleSignIn = GoogleSignIn();
     try {
       var user = await googleSignIn.signIn();
       if (user != null) {
-        debugPrint('======${user.displayName}');
-        debugPrint('======${user.email}');
-        debugPrint('======${user.photoUrl}');
+        Map<String, dynamic> userCredintial = {
+          "username": user.displayName.toString(),
+          "email": user.email.toString(),
+          "profileImage": user.photoUrl.toString(),
+          "fcpmToken": "akn nai pore dimo"
+        };
+
+        final response = await NetworkCaller().postRequest(
+            Utils.baseUrl + Utils.googleLogin,
+            body: userCredintial);
+
+        if (response.isSuccess) {
+          preferencesHelper.setString(
+              "userToken", response.responseData['accessToken']);
+          preferencesHelper.setString("userId", response.responseData['id']);
+          debugPrint("======login===Succes");
+          debugPrint("======name===${user.displayName}");
+          debugPrint("======email===${user.email}");
+          debugPrint("======photo===${user.photoUrl}");
+        }
       }
     } catch (e) {
       debugPrint(e.toString());
