5d50b24901af07a60f78f6237ea23e1b53831c84
Md Sumon <109859876+Mdsumon743@users.noreply.github.com>
2025-01-10 16:46:57 +0600

    push code

--- lib/feature/payment/controller/payment_controller.dart
+++ lib/feature/payment/controller/payment_controller.dart
@@ -2,8 +2,7 @@ import 'package:get/get.dart';
 import 'package:unforgettable_getaway/core/utils/assetpath.dart';
 class PaymentController extends GetxController{
   var selectedPayment="".obs;
-  var publickey = "pk_test_51QKdoWHlqANa4h6mpxHAu85KhAFt2fnBsRRFKeWUhdqmlZJQ0SI6b3ZCuZ0LU3lCu7Gc4qT6rdl8xVsGqntH2G0400sW14ZlIt";
-  var secretkey = "sk_test_51QKdoWHlqANa4h6mz1D3tXUHzKCKZhiMCghWis6YXvF2zIdyr51X8Bm4bkiFfuxSIlQf8s9FSLoSQlXnbe3htsTU00r2qjdSn1";
+
   var img= Assetpath.creditcard.obs;
   void selectPlan(String izaz){
     selectedPayment.value=izaz;
@@ -22,5 +21,9 @@ class PaymentController extends GetxController{
     }else{
    img.value=Assetpath.pay;
     }
+  }
+
+  Future<void>makePayment(double amount, String currency)async{
+    
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
 
 import 'package:unforgettable_getaway/app.dart';
 import 'package:unforgettable_getaway/core/binding/binding.dart';
 import 'package:unforgettable_getaway/feature/notification/controller/notification_controller.dart';
 import 'package:unforgettable_getaway/feature/onboarding/controller/onboarding_controller.dart';
+import 'package:unforgettable_getaway/feature/payment/controller/key.dart';
 
 import 'core/helper/shared_prefarences_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = publickey;
+  Stripe.instance;
   await Firebase.initializeApp();
   AppBingding().dependencies();
   OnboardingController().onInit();
